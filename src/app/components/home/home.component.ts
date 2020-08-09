bc75e25d40e8665cbc20b1650c6a920ccfd5d8d1
jmoragarios <jmoragarios@gmail.com>
2020-08-09 16:46:31 -0600

    Added error message block at home view

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,13 +12,28 @@ export class HomeComponent implements OnInit {
 
   newestSongs: any[] = [];
   loading: boolean;
+  error: boolean;
+  errorMessage: string;
 
   constructor( private spotify: SpotifyService ) {
+
     this.loading = true;
+    this.error = false;
+
     this.spotify.getNewReleases().subscribe( (data: any) => {
+
       this.newestSongs = data;
       this.loading = false;
+
+    }, (serviceError) => {
+
+      this.errorMessage = serviceError.error.error.message;
+
+      this.loading = false;
+      this.error = true;
+
     });
+
    }
 
   ngOnInit(): void {
